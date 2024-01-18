5f84cbbe613daebda9c7ec6b4cf1fe22f602fdce
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2024-01-18 12:56:11 -0500

    my information v1

---
+++ MyInfo.py
@@ -0,0 +1,6 @@
+#CS175L
+#Miriam Abecasis
+#myInfo
+print("My name is: Miriam Abecasis")
+print("My address is: 1234 Central Ave, Bismark, North Dakota, 12345")
+print("My phone number is 123-456-7890")
