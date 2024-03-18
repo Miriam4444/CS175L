1bccd5f97bcb4dfbf7a7c9ea22a98d79ee4d1fe7
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2024-03-18 17:07:29 -0400

    code with exceptions:)

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -4,13 +4,21 @@
 def main():
     total_lines = 0
     total_num = 0
-    infile = open ('numbers.txt' , 'r')
-    #read values
-    for line in infile:
-        total_lines += 1
-        total_num += float(line)
-        print(f'I read in {total_lines} number(s) Current number is: {float(line):.2f} Total is: {total_num:.2f}')
-    print(f'Average: {(total_num / total_lines)}')
+    try:
+        infile = open ('numbers.txt' , 'r')
+        #read values
+        for line in infile:
+            try:
+                total_num += int(line)
+                total_lines += 1
+                print(f'I read in {total_lines} number(s) Current number is: {float(line):.2f} Total is: {total_num:.2f}')
+            except ValueError:
+                print(f'Bad data: {line.strip()} skipping')
+        print(f'Average: {(total_num / total_lines)}')
+    except IOError:
+        print("File not found: numbers.txt - exiting")
+    #except ValueError:
+     #   print(f'Bad data: {line} skipping')
 
 if __name__ == '__main__':
     main()
